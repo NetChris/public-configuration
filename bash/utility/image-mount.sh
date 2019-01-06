da57e6e5fac4fa1a35c0052e53b6f16a63a7198c
Chris Simmons <chris.dev@netchris.com>
2019-01-05 17:21:31 -0800

    image-mount.sh
    With executable flag - "git update-index --chmod=+x image-mount.sh"

---
+++ bash/utility/image-mount.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Usage: image-mount.sh <image-filename> <W95|Linux>
+# W95 = Boot Partition / Linux = Root Partition
+# 2019-01-05 - Taken from:
+# https://www.raspberrypi.org/forums/viewtopic.php?p=1192933#p1192933
+
+IMG="$1"
+PART="$2"
+
+INFO="$(fdisk -lu "${IMG}")"
+START="$(grep "${PART}" <<< "${INFO}" | awk '{print $2}')"
+SIZE="$(grep "${PART}" <<< "${INFO}" | awk '{print $4}')"
+LOOP="$(losetup -f --show -o $((${START} * 512)) --sizelimit $((${SIZE} * 512)) "${IMG}")"
+mount "${LOOP}" /mnt/
+echo ""
+echo "${IMG} mounted on /mnt/"
+echo ""
+echo "When done, run:"
+echo "umount /mnt/"
+echo "losetup -d ${LOOP}"
+echo ""
